d1e568c2fe9f9b4fd00db13d4139e40b5481eee0
石渡拓海 <t-ishiwata@sight-visit.com>
2020-12-14 00:09:33 +0900

    [Add] Add TimeCardComponent in CardList

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
+
+import TimeCard from './TimeCard';
 
 const POSTS = gql`
   query {
@@ -42,6 +38,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 const CardList = () => {
   const { data } = useQuery(POSTS, {});
+
+  // setInterval(() => {
+  //   setCount(count + 1);
+  // }, 1000);
   const classes = useStyles();
   data && console.log(data.posts);
   return (
@@ -50,27 +50,7 @@ const CardList = () => {
         {data &&
           data.posts.map((post) => (
             <Grid item key={post.id} xs={12} sm={6} md={4}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title="Image title"
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {post.title}
-                  </Typography>
-                  <Typography>{post.description}</Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    View
-                  </Button>
-                  <Button size="small" color="primary">
-                    Edit
-                  </Button>
-                </CardActions>
-              </Card>
+              <TimeCard title={post.title} description={post.description} createdAt={post.createdAt} />
             </Grid>
           ))}
       </Grid>
---
+++ src/components/TimeCard.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Countdown from './CountDown';
+
+const useStyles = makeStyles((theme) => ({
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+const TimeCard = (props) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="Image title" />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {props.title}
+        </Typography>
+        <Typography>{props.description}</Typography>
+        <Typography>{new Date(props.createdAt).toLocaleString()}</Typography>
+        <Countdown endTime={new Date('12/25/2020').getTime()} />
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          View
+        </Button>
+        <Button size="small" color="primary">
+          Edit
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default TimeCard;
