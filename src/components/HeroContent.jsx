f18e592ac49865e71a52ffc1d4739d6da7310b58
Watakumi <watauta22@gmail.com>
2020-12-23 04:26:57 +0900

    [Change] Hero Content Text

--- src/components/HeroContent.jsx
+++ src/components/HeroContent.jsx
@@ -26,8 +26,7 @@ const HeroContent = () => {
           Welcome To BosoBoso
         </Typography>
         <Typography variant="h5" align="center" color="textSecondary" paragraph>
-          Something short and leading about the collection below—its contents, the creator, etc. Make it short and
-          sweet, but not too short so folks don&apos;t simply skip over it entirely.
+          Enjoy!
         </Typography>
         <div className={classes.heroButtons}>
           <Grid container spacing={2} justify="center">
